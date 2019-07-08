8955698d2766351d6d4993882f0563a57970c1c9
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-07-08 23:39:01 +0600

    ..

--- models/user.js
+++ models/user.js
@@ -1,17 +1,38 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const {
+  Schema
+} = mongoose;
 
 // creat user schema
 const userSchema = new Schema({
-  name: {type: String,required: true},
-  email: { type: String,required: true},
-  password: {type: String,required: true},
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   avatar: String,
-  date: {type: Date, default: Date.now},
-  isAdmin: {type: Boolean, default: false},
-  questions: [{type: Schema.Types.ObjectId, ref: 'questions'}]
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  questions: [{
+    type: Schema.Types.ObjectId,
+    ref: 'questions'
+  }]
 });
 
 // with user schema , load the user model for User collection
 const User = mongoose.model("users", userSchema);
 module.exports = User;
+// can i create an app which will be used by 1000 of users in the future ?
